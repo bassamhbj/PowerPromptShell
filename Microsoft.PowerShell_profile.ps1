959219bfb0fb1696fc9fe5e5fa8323a3c91311e7
bassamhbj <hbjproduction@gmail.com>
2022-03-23 11:51:13 +0900

    Script refactor - Change Date and command prompt view

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,26 +4,22 @@
 $GIT_GLYPH = $([char]0xf113)
 $BRANCH_GLYPH = $([char]0xe0a0)
 $PYTHON_GLYPH = $([char]0xe235)
-$SEPARATOR_GLYPH = $([char]0xe0b0)
+$SEPARATOR_RIGHT_GLYPH = $([char]0xe0b0)
+$SEPARATOR_LEFT_GLYPH = $([char]0xe0b2)
 $FOLDER_GLYPH = $([char]0xf115)
 $WARNING_GLYPH = $([char]0xf071)
+$PROMPT_GLYPH = $([char]0xe0b1)
 
-function prompt {
-    # Set Window Title
-    $host.ui.RawUI.WindowTitle = "Path: $pwd"
-
-    # Configure Output Info
-    $currentFolder = Split-Path -Path $pwd -Leaf
-    #$user = [Security.Principal.WindowsIdentity]::GetCurrent();
-    $date = Get-Date -Format 'dddd hh:mm:ss tt'
-
-    # Check If is Admin
+function GetAdminDetails {
     $isAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+ 
+    return $(if ($isAdmin) { " Admin:Enabled " } else { " Admin:Disabled " })
+}
 
+function GetGitRepoDetails {
     # Check if git is enabled in the current directory
     $isGitRepo = git rev-parse --git-dir
-    $repoName = "----"
-    $branchName = ""
+    $repoDetails = "----"
 
     if($isGitRepo){
         $repo = git config --get remote.origin.url
@@ -31,58 +27,141 @@ function prompt {
         if($repo) {
             $repoName = $repo.split("/") | Select-Object -Last 1
             $branch = git branch --show-current
-            $branchName = "$BRANCH_GLYPH $branch"
+            $repoDetails = "$repoName $BRANCH_GLYPH $branch"
         } else {
             $repoName = "$WARNING_GLYPH GIT NOT INITIATED $WARNING_GLYPH"
         }
-    }    
+    }
+    
+    return " $GIT_GLYPH $repoDetails"
+}
+
+function GetFolderDetails {
+    $currentFolder = Split-Path -Path $pwd -Leaf
+
+    return " $FOLDER_GLYPH $currentFolder "
+}
+
+function GetDateDetails {
+    $date = Get-Date -Format 'dddd hh:mm:ss tt'
+    
+    return " $date "
+}
+
+function GetEmptySeparator {
+    param (
+        [int]$PromptLength,
+        [int]$DateLength
+    )
+    
+    $promptTotalLength = $host.ui.RawUI.WindowSize.Width
+    $emptyStringLength = $promptTotalLength - $PromptLength - $DateLength
+
+    $emptySeparator = ""
+
+    for ($i = 0; $i -lt $emptyStringLength; $i++) {
+        $emptySeparator += " "
+    }
+
+    return $emptySeparator
+}
+
+function prompt {
+    # Set Window Title
+    $host.ui.RawUI.WindowTitle = "Path: $pwd"
+
+    $promptLength = 0
+
+    # Configure Output Info
+    $currentFolderDetails = GetFolderDetails
+    $dateDetails = GetDateDetails
+    $adminDetails = GetAdminDetails
+    $repoDetails = GetGitRepoDetails
+
+    $promptLength += $currentFolderDetails.Length
+    $promptLength += $adminDetails.Length
+    $promptLength += $repoDetails.Length
+
+    # Use 3 separators
+    $promptLength += $SEPARATOR_RIGHT_GLYPH.Length
+    $promptLength += $SEPARATOR_RIGHT_GLYPH.Length
+    $promptLength += $SEPARATOR_RIGHT_GLYPH.Length
+
+    # Date separators
+    $promptLength += $SEPARATOR_RIGHT_GLYPH.Length
+    $promptLength += $SEPARATOR_LEFT_GLYPH.Length
+
 
     # If is a Conda Environment, display current environment name
     if($env:CONDA_DEFAULT_ENV) {
-        Write-Host " $PYTHON_GLYPH $($env:CONDA_DEFAULT_ENV) " `
+        $codaDetails = " $PYTHON_GLYPH $($env:CONDA_DEFAULT_ENV) "
+
+        $promptLength += $codaDetails.Length
+
+        Write-Host $codaDetails `
             -BackgroundColor Cyan `
             -ForegroundColor Black `
             -NoNewline
     } 
 
+    $emptySeparator = GetEmptySeparator `
+        -PromptLength $promptLength `
+        -DateLength $dateDetails.Length
+
     # Display if run as Administrator
-    Write-host ($(if ($isAdmin) { " Admin:Enabled " } else { " Admin:Disabled " })) `
+    Write-host $adminDetails `
         -BackgroundColor DarkRed `
         -ForegroundColor White `
         -NoNewline
 
     # Separator
-    Write-Host $SEPARATOR_GLYPH `
+    Write-Host $SEPARATOR_RIGHT_GLYPH `
         -BackgroundColor DarkBlue `
         -ForegroundColor DarkRed `
         -NoNewline
 
     # Current folder
-    Write-Host " $FOLDER_GLYPH $currentFolder " `
+    Write-Host $currentFolderDetails `
         -BackgroundColor DarkBlue `
         -ForegroundColor Black `
         -NoNewline
 
     # Separator
-    Write-Host $SEPARATOR_GLYPH `
+    Write-Host $SEPARATOR_RIGHT_GLYPH `
         -BackgroundColor Yellow `
         -ForegroundColor DarkBlue `
         -NoNewline
 
     # Git repo
-    Write-Host " $GIT_GLYPH $repoName $branchName" `
+    Write-Host $repoDetails `
         -BackgroundColor Yellow `
         -ForegroundColor Black `
         -NoNewline
 
     # Separator
-    Write-Host $SEPARATOR_GLYPH `
+    Write-Host $SEPARATOR_RIGHT_GLYPH `
         -ForegroundColor Yellow `
         -NoNewline
 
+    Write-Host $emptySeparator `
+        -NoNewline
+    
     # Date
-    Write-Host " $date" `
-        -ForegroundColor White
+    Write-Host $SEPARATOR_LEFT_GLYPH `
+        -ForegroundColor DarkGray `
+        -NoNewline
 
-    return "> "
+    Write-Host $dateDetails `
+        -ForegroundColor White `
+        -BackgroundColor DarkGray `
+        -NoNewline
+
+    Write-Host $SEPARATOR_RIGHT_GLYPH `
+        -ForegroundColor DarkGray
+
+    Write-Host "$PROMPT_GLYPH$PROMPT_GLYPH" `
+        -ForegroundColor DarkYellow `
+        -NoNewline
+
+    return " "
 } 
