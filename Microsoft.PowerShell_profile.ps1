a63e663fbe50eb244de7d724db6a2a47d243e2dc
bassamhbj <hbjproduction@gmail.com>
2022-11-16 16:10:41 +0900

    Update Profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,20 @@
 # Comment out if use Terminal-Icons
 #Import-Module -Name Terminal-Icons
 
+Write-Host "            //////////////////////////////////// " -ForegroundColor Blue
+Write-Host "           ///////   //////////////////////////  " -ForegroundColor Blue
+Write-Host "          /////////     //////////////////////   " -ForegroundColor Blue
+Write-Host "         /////////////    ///////////////////    " -ForegroundColor Blue
+Write-Host "        ////////////////    ////////////////     " -ForegroundColor Blue
+Write-Host "       //////////////////     /////////////      " -ForegroundColor Blue
+Write-Host "      ///////////////      ///////////////       " -ForegroundColor Blue
+Write-Host "     /////////////     //////////////////        " -ForegroundColor Blue
+Write-Host "    //////////      ////         ///////         " -ForegroundColor Blue
+Write-Host "   ///////////  ///////////////////////          " -ForegroundColor Blue
+Write-Host "  ////////////////////////////////////           " -ForegroundColor Blue
+""
+""
+
 $GIT_GLYPH = $([char]0xf113)
 $BRANCH_GLYPH = $([char]0xe0a0)
 $PYTHON_GLYPH = $([char]0xe235)
@@ -9,6 +23,19 @@ $SEPARATOR_LEFT_GLYPH = $([char]0xe0b2)
 $FOLDER_GLYPH = $([char]0xf115)
 $WARNING_GLYPH = $([char]0xf071)
 $PROMPT_GLYPH = $([char]0xe0b1)
+#$TERMINAL_GLYPH = $([char]0xf489)
+$TERMINAL_GLYPH = $([char]0xe62a)
+$START_GLYPH = "$([char]0x256d)$([char]0x2500)"
+$START_GLYPH_2 = "$([char]0x2570)$([char]0x2500)"
+
+
+$CIRCLE_LEFT_GLYPH = $([char]0xe0b6)
+$CIRCLE_RIGHT_GLYPH = $([char]0xe0b4)
+#$ARC_RIGHT_GLYPH = $([char]0xe0b5)
+
+function GetPromptIcon {
+    return " $(if($env:CONDA_DEFAULT_ENV) { $PYTHON_GLYPH } else { $TERMINAL_GLYPH } )  "
+}
 
 function GetAdminDetails {
     $isAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
@@ -77,10 +104,17 @@ function prompt {
     $dateDetails = GetDateDetails
     $adminDetails = GetAdminDetails
     $repoDetails = GetGitRepoDetails
+    $promptIcon = GetPromptIcon
 
     $promptLength += $currentFolderDetails.Length
-    $promptLength += $adminDetails.Length
+    #$promptLength += $adminDetails.Length
     $promptLength += $repoDetails.Length
+    $promptLength += $promptIcon.Length
+
+    # Icon separators
+    $promptLength += $CIRCLE_LEFT_GLYPH.Length
+    $promptLength += $CIRCLE_RIGHT_GLYPH.Length
+    $promptLength += $CIRCLE_RIGHT_GLYPH.Length
 
     # Use 3 separators
     $promptLength += $SEPARATOR_RIGHT_GLYPH.Length
@@ -91,34 +125,71 @@ function prompt {
     $promptLength += $SEPARATOR_RIGHT_GLYPH.Length
     $promptLength += $SEPARATOR_LEFT_GLYPH.Length
 
+    # Display start
+    $promptLength += $START_GLYPH.Length
+
+    Write-Host $START_GLYPH `
+        -ForegroundColor Yellow `
+        -NoNewline
+
+    # Display terminal icon
+    Write-Host $CIRCLE_LEFT_GLYPH `
+        -ForegroundColor Yellow `
+        -NoNewline
+
+    Write-Host $promptIcon `
+        -BackgroundColor Yellow `
+        -ForegroundColor Black `
+        -NoNewline
+
+    Write-Host $CIRCLE_RIGHT_GLYPH `
+        -BackgroundColor DarkBlue `
+        -ForegroundColor Yellow `
+        -NoNewline
 
     # If is a Conda Environment, display current environment name
     if($env:CONDA_DEFAULT_ENV) {
-        $codaDetails = " $PYTHON_GLYPH $($env:CONDA_DEFAULT_ENV) "
+        $promptLength += $env:CONDA_DEFAULT_ENV.Length
 
-        $promptLength += $codaDetails.Length
-
-        Write-Host $codaDetails `
+        Write-Host $CIRCLE_RIGHT_GLYPH `
             -BackgroundColor Cyan `
             -ForegroundColor Black `
             -NoNewline
-    } 
+
+        Write-Host $env:CONDA_DEFAULT_ENV `
+            -BackgroundColor Cyan `
+            -ForegroundColor Black `
+            -NoNewline
+    } else {
+        # Write-Host $CIRCLE_RIGHT_GLYPH `
+        #     -BackgroundColor DarkRed `
+        #     -ForegroundColor Black `
+        #     -NoNewline
+
+        # # Black separator
+
+        # Write-Host $CIRCLE_RIGHT_GLYPH `
+        #     -BackgroundColor DarkBlue `
+        #     -ForegroundColor Black `
+        #     -NoNewline
+    }
 
     $emptySeparator = GetEmptySeparator `
         -PromptLength $promptLength `
         -DateLength $dateDetails.Length
 
     # Display if run as Administrator
-    Write-host $adminDetails `
-        -BackgroundColor DarkRed `
-        -ForegroundColor White `
-        -NoNewline
+
+    # Write-host $adminDetails `
+    #     -BackgroundColor DarkRed `
+    #     -ForegroundColor White `
+    #     -NoNewline
 
     # Separator
-    Write-Host $SEPARATOR_RIGHT_GLYPH `
-        -BackgroundColor DarkBlue `
-        -ForegroundColor DarkRed `
-        -NoNewline
+    # Write-Host $SEPARATOR_RIGHT_GLYPH `
+    #     -BackgroundColor DarkBlue `
+    #     -ForegroundColor DarkRed `
+    #     -NoNewline
 
     # Current folder
     Write-Host $currentFolderDetails `
@@ -159,9 +230,14 @@ function prompt {
     Write-Host $SEPARATOR_RIGHT_GLYPH `
         -ForegroundColor DarkGray
 
-    Write-Host "$PROMPT_GLYPH$PROMPT_GLYPH" `
+    ## Second line
+    Write-Host $START_GLYPH_2 `
         -ForegroundColor DarkYellow `
         -NoNewline
+
+    # Write-Host "$PROMPT_GLYPH$PROMPT_GLYPH" `
+    #     -ForegroundColor DarkYellow `
+    #     -NoNewline
 
     return " "
 } 
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # PowerPromptShell
+
+## How to install the PowerShell Profile
+1. Get PowerShell Profile folder path executing the following command:
+`` $PROFILE ``
+2. Copy the PowerPromptShell Profile to that folder
+3. Install Required font: `Hack Nerd Font`
